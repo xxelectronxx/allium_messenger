4ceabede03a33815526b9be72c932733e98ab61c
Ephram Kukartsev <ephram.kukartsev@gmail.com>
2021-12-28 00:28:11 -0800

    added connection.py

---
+++ allium_messenger/connection.py
@@ -0,0 +1,57 @@
+from stem.control import Controller
+from flask import Flask
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+class AlliumConnection:
+
+    def __init__(self, hidden_svc_dir, app_name='example', port=5000, host='127.0.0.1', control_port=9051):
+        self.app = Flask(app_name)
+        self.port = port
+        self.host = host
+        self.control_port = control_port
+        self.hidden_svc_dir = f"/var/lib/tor/{hidden_svc_dir}"
+        self.controller = None
+        self.service_name = None
+
+        @self.app.route('/')
+        def index():
+            return "<h1>Tor works!</h1>"
+        self.index = index
+
+        @self.app.route('/allium')
+        def request():
+            return "<h1>request URL works!</h1>"
+        self.request = request
+
+
+    def get_service_name(self):
+        self.service_name = '7tcowwy2zjdfed4vdmooh2267i2qxzgw6jldgky7rmhnpoaxth5wahad.onion'
+        return self.service_name
+
+
+
+
+    def create_service(self):
+        logger.info(" * Getting controller")
+        self.controller = Controller.from_port(address=self.host, port=self.control_port)
+        try:
+            self.controller.authenticate(password="test_password")
+            self.controller.set_options([
+                ("HiddenServiceDir", self.hidden_svc_dir),
+                ("HiddenServicePort", "80 %s:%s" % (self.host, str(self.port)))
+            ])
+            svc_name = self.get_service_name()
+            logger.info(" * Created host: %s" % svc_name)
+        except Exception as e:
+            print(e)
+            logger.info(f'OHMYGOD!!!! {e}')
+        self.app.run()
+
+
+if __name__ == "__main__":
+    my_connection = AlliumConnection(hidden_svc_dir='hidden_service')
+    my_connection.create_service()
--- examples/exampleservice.py
+++ examples/exampleservice.py
@@ -1,3 +1,4 @@
+import stem
 from stem.control import Controller
 from flask import Flask
 import logging
@@ -10,22 +11,24 @@ if __name__ == "__main__":
     app = Flask("example")
     port = 5000
     host = "127.0.0.1"
-    hidden_svc_dir = "c:/temp/"
+    hidden_svc_dir = "/var/lib/tor/hidden_service"
 
     @app.route('/')
     def index():
         return "<h1>Tor works!</h1>"
+
     print(" * Getting controller")
-    controller = Controller.from_port(address="127.0.0.1", port=9151)
+    controller = Controller.from_port(address="127.0.0.1", port=9051)
     try:
-        controller.authenticate(password="")
+        controller.authenticate(password="test_password")
         controller.set_options([
             ("HiddenServiceDir", hidden_svc_dir),
             ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
             ])
-        svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
+        #svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
+        svc_name = stem.control.Controller.get_hidden_service_descriptor()
         print(" * Created host: %s" % svc_name)
-        logger.info(" * Created host: %s" % svc_name)
+        #logger.info(" * Created host: %s" % svc_name)
     except Exception as e:
         print(e)
         logger.info(f'OHMYGOD!!!! {e}')
